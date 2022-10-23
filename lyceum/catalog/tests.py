765139142f4f924d28b2f7ab17c568a3c2332c30
Grigory <wileere@yandex.ru>
2022-10-23 17:45:12 +0700

    Change TestCase

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -7,19 +7,21 @@ class StaticURLTests(TestCase):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
 
+
+class RegularExpressionsTests(TestCase):
+    # Правильные запросы
     def test_catalog_pk_true_endpoint(self):
         # Делаем запрос к каталогу по целому числу
         # И проверяем статусы
 
-        # Правильные запросы
         response = Client().get('/catalog/123/')
         self.assertEqual(response.status_code, 200)
 
         response = Client().get('/catalog/1/')
         self.assertEqual(response.status_code, 200)
 
+    # Неправильные запросы
     def test_catalog_pk_false_endpoint(self):
-        # Неправильные запросы
         response = Client().get('/catalog/abc/')
         self.assertEqual(response.status_code, 404)
 
