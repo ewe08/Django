1f29379bd69bf94831d7152d84a01ee287ff2e9d
Grigory <wileere@yandex.ru>
2022-10-31 12:11:58 +0700

    Make connection

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from .validators import validate_must_be_param
 
 
-class Item(models.Model):
-    is_published = models.BooleanField(default=True)
-    name = models.CharField(max_length=150)
-    text = models.TextField(validators=[validate_must_be_param])
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=150)
     is_published = models.BooleanField(default=True)
@@ -19,3 +13,11 @@ class Category(models.Model):
     is_published = models.BooleanField(default=True)
     weight = models.PositiveSmallIntegerField(default=100)
     slug = models.SlugField(max_length=200, unique=True)
+
+
+class Item(models.Model):
+    is_published = models.BooleanField(default=True)
+    name = models.CharField(max_length=150)
+    text = models.TextField(validators=[validate_must_be_param])
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, on_delete=models.CASCADE)
