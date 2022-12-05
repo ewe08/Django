43ea2913ac50f1d53a6a620844139e4b768db54e
Андрей <andrej5122785@yandex.ru>
2022-12-05 19:28:09 +0300

    item detail add rating form and rating info

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Avg
+from django.urls import reverse
 
 from .models import Item, Photo
+from rating.models import Rating
+from rating.forms import RatingForm
 
 
 def item_list(request):
@@ -16,11 +20,40 @@ def item_list(request):
 
 def item_detail(request, pk: int):
     template = 'catalog/index_detail.html'
+
     item = get_object_or_404(
         Item.objects.published(),
         pk=pk,
     )
+
+    rating = Rating.objects.filter(item=item, user=request.user).first()
+    if rating:
+        user_rating = rating.rate
+    else:
+        user_rating = None
+    count = Rating.objects.filter(item=pk).count()
+    average_rating = Rating.objects.filter(item=pk).aggregate(Avg('rate'))['rate__avg']
+    form = RatingForm(request.POST or None, instance=rating)
     context = {'title': 'Подробнее',
                'item': item,
-               'photos': Photo.objects.filter(item_galery=pk)}
+               'photos': Photo.objects.filter(item_galery=pk),
+               'rating_count': count,
+               'average_rating': average_rating,
+               'btn_label': 'Отправить',
+               'user_rating': user_rating,
+               'form': form}
+
+    if request.method == 'POST' and form.is_valid():
+        if rating:
+            rating.rate = form.cleaned_data['rate']
+        else:
+            rating = Rating.objects.create(
+               user=request.user,
+               item=item,
+               rate=form.cleaned_data['rate']
+            )
+        rating.save()
+
+        return redirect(reverse('catalog:item_detail', args=[pk]))
+
     return render(request, template, context)
